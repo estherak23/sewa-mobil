8b3eb14580be7bf9a33000879a6787a5766d20c5
estherak23 <70134970+estherak23@users.noreply.github.com>
2023-06-14 16:02:06 +0700

    migrate db,model,middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboadController;
+use App\Http\Controllers\Admin\BrandController as AdminBrandController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('front.index');
 
-Route::middleware([
+Route::prefix('admin')->name('admin.')->middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
+    'admin'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [AdminDashboadController::class,'index'] )->name('dashboard');
+    Route::resource('brands', AdminBrandController::class);
 });
